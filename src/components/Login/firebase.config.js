7377588f71946281119c938e9b3bd36ae4a7f00f
Nishan <arifuzzamannishan@gmail.com>
2021-03-28 02:37:14 +0600

    finish

---
+++ src/components/Login/firebase.config.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyB4ag8Vax0iCoYEBRLpkRdSNglmiR86o7A",
+  authDomain: "burj-al-aarabb.firebaseapp.com",
+  projectId: "burj-al-aarabb",
+  storageBucket: "burj-al-aarabb.appspot.com",
+  messagingSenderId: "962527063505",
+  appId: "1:962527063505:web:6aeb0222e06de3f1a544f0"
+  };
+  export default firebaseConfig;
